fda984ff4d71c12e76a26224b8e13838a6b88104
audrickkena <audrickkena@gmail.com>
2024-06-22 19:19:04 +0800

    solutions updated

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,27 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    ''' 
+    since fave_numbers cannot contain duplicates, calculating
+    the difference between the length of 
+    fave_numbers_1 and the length of the set containing 
+    fave_numbers_1 and fave_numbers_2 can determine the match
+
+    i.e [1, 2, 3, 4], [2, 3] into a set would just be [1, 2, 3, 4]
+    which is the length of the first list
+    '''
+    allFave = set(fave_numbers_1 + fave_numbers_2)
+    if len(fave_numbers_1) == len(allFave):
+        return True
+    return False
+
+    '''
+    ORIGINAL CODE
+
     for number in fave_numbers_2:
         if number not in fave_numbers_1:
             return False
 
     return True
+    
+    '''
--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,97 @@ def search_users(args):
     """
 
     # Implement search here!
+    
+    # list to store results
+    resList = []
 
-    return USERS
+    # No queries, list all 
+    if len(args) == 0:
+        return USERS
+    
+    else:
+        keyVals = getKeys(args)
+        if 'id' in keyVals:
+            # run idSearch
+            res = idSearch(args[keyVals['id']], USERS)
+            if res != None:
+                resList.append(res)
+        if 'name' in keyVals:
+            # run nameSearch
+            res = nameSearch(args[keyVals['name']], USERS)
+            if res != None:
+                for e in res:
+                    if e in resList:
+                        continue
+                    resList.append(e)
+        if 'age' in keyVals:
+            # run ageSearch
+            res = ageSearch(args[keyVals['age']], USERS)
+            if res != None:
+                for e in res:
+                    if e in resList:
+                        continue
+                    resList.append(e) 
+        if 'occupation' in keyVals:
+            # run occSearch
+            res = occSearch(args[keyVals['occupation']], USERS)
+            if res != None:
+                for e in res:
+                    if e in resList:
+                        continue
+                    resList.append(e)
+        if len(resList) > 0:
+            return resList
+        else:
+            return 'No entry matches your query! Try again with values in the database\n(Try searching with no query to get whole database)'
+        
+# function for getting keys in query dict and storing into a key dict, making sure each key
+# in the key dict is lower cased and each corresponding value is the key for the query dict       
+def getKeys(args):
+    k = args.keys()
+    loweredK = {}
+    for e in k:
+        loweredK[e.lower()] = e
+    return loweredK
+
+# function for searching by ID, parameter (n) is str only, parameter (data) is list of 
+# user dicts
+def idSearch(n, data):
+    for e in data:
+        if e['id'] == n:
+            return e
+    return None
+
+# function for searching by name, parameter (s) is str only, parameter (data) is list of
+# user dicts
+def nameSearch(s, data):
+    resList = []
+    for e in data:
+        if s.lower() in e['name'].lower():
+            resList.append(e)
+    if len(resList) == 0:
+        return None
+    return resList
+
+# function for searching by age, parameter (n) is str only, parameter (data) is list of
+# user dicts
+def ageSearch(n, data):
+    resList = []
+    ageRange = [int(n) - 1, int(n), int(n) + 1]
+    for e in data:
+        if e['age'] in ageRange:
+            resList.append(e)
+    if len(resList) == 0:
+        return None
+    return resList
+
+# function for searching by occupation, parameter (s) is str only, parameter (data) is list of
+# user dicts
+def occSearch(s, data):
+    resList = []
+    for e in data:
+        if s.lower() in e['occupation'].lower():
+            resList.append(e)
+    if len(resList) == 0:
+        return None
+    return resList
